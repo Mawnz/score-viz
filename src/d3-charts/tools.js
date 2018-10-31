5298a8a2d5a358682ead94712993125c58080bf8
Mawnz <mansab@kth.se>
2018-10-31 21:51:18 +0100

    aa

--- src/d3-charts/tools.js
+++
@@ -1,8 +0,0 @@
-/* https://javascriptweblog.wordpress.com/2011/08/08/fixing-the-javascript-typeof-operator/ */
-export function toType(obj) {
-	return ({}).toString.call(obj).match(/\s([a-zA-Z]+)/)[1].toLowerCase();
-}
-
-export function something() {
-	return 'something';
-}
